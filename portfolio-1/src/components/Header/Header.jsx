6540e75f0238943f9d8dffcd29f95d63e74f5783
Reggie P <reginaldpitts1@gmail.com>
2023-06-25 10:47:13 -0500

    Edited css for header socials and img, deleted 3 photos

--- portfolio-1/src/components/Header/Header.jsx
+++ portfolio-1/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 import CTA from './CTA'
-import ME from '../../assets/Image3.jpg'
+import ME from '../../assets/Image1.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
