43c978ca6995780c7cd5b3651bba84cfb996002a
Reggie P <reginaldpitts1@gmail.com>
2023-06-25 21:27:29 -0500

    imported icons for nav bar

--- portfolio-1/src/components/Nav/Nav.jsx
+++ portfolio-1/src/components/Nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {GiBlackBook} from 'react-icons/gi'
+import {RiServiceLine} from 'react-icons/ri'
+import {TiContacts} from 'react-icons/ti'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#experience"><GiBlackBook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#contact"><TiContacts /></a>
+    </nav>
   )
 }
 
