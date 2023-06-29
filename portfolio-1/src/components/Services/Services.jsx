3901a59bee71c39355ecc124928213807ca21269
Reggie P <reginaldpitts1@gmail.com>
2023-06-29 15:25:44 -0500

    added html code including ul/li lists for services.jsx

--- portfolio-1/src/components/Services/Services.jsx
+++ portfolio-1/src/components/Services/Services.jsx
@@ -1,9 +1,128 @@
 import React from 'react'
 import './Services.css'
+import {LiaCheckCircle} from 'react-icons/lia'
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services_container">
+        <article className ="service">
+          
+          <div className="service_head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+          </ul>
+
+        </article>
+        {/* END OF UI/UX */}
+
+        <article className ="service">
+          
+          <div className="service_head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+          </ul>
+
+        </article>
+        {/* END OF WEB DEVELOPMENT */}
+
+        <article className ="service">
+          
+          <div className="service_head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service_list">
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+
+            <li>
+              <LiaCheckCircle className="service_list-icon" />
+              <p>Insert Project Name Here</p>
+            </li>
+          </ul>
+
+        </article>
+        {/* END OF CONTENT CREATION */}
+      </div>
+    </section>
   )
 }
 
