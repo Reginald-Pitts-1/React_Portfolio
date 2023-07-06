8437a2b1ebc2b11e56010cbba39d967e0087c721
Reggie P <reginaldpitts1@gmail.com>
2023-07-06 09:31:26 -0500

    added emailjs functions into contact.jsx

--- portfolio-1/src/components/Contact/Contact.jsx
+++ portfolio-1/src/components/Contact/Contact.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import './Contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerFill} from 'react-icons/ri'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3k7kxa8', 'template_g1tpqzk', form.current, 'PNF8k-Jyg0jGoJ-WF')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -29,7 +46,7 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows="7" placeholder="Your Message" required />
